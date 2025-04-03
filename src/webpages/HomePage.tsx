d04fa464c478c76b24dc73cb0e45f5ad14c4cd74
Josh Chelen <jchelen@udel.edu>
2025-04-03 13:21:20 -0400

    refactored API Key handling to its own function

--- src/webpages/HomePage.tsx
+++ src/webpages/HomePage.tsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 
+type APIKeyFormProps = {
+    handleSubmit: () => void;
+    changeKey: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+function APIKeyForm({ handleSubmit, changeKey }: APIKeyFormProps): React.JSX.Element {
+    return (
+        <Form>
+            <Form.Label>API Key:</Form.Label>
+            <Form.Control 
+                type="password" 
+                placeholder="Insert API Key Here" 
+                onChange={changeKey} 
+            />
+            <br />
+            <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+        </Form>
+    );
+}
+
 export function HomePage({ 
     handleSubmit, 
-    changeKey, 
-}: { 
-    handleSubmit: () => void; 
-    changeKey: (e: React.ChangeEvent<HTMLInputElement>) => void; 
-}): React.JSX.Element {
+    changeKey 
+}: APIKeyFormProps): React.JSX.Element {
     return (
         <div>
-            <h1>
-                Home Page
-            </h1>
-            <Form>
-                <Form.Label>API Key:</Form.Label>
-                <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-                <br></br>
-                <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-            </Form>
+            <h1>Home Page</h1>
+            <APIKeyForm handleSubmit={handleSubmit} changeKey={changeKey} />
         </div>
     );
 }

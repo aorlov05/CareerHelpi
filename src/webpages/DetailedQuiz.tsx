ef2bced1245a1321966ad6abbceda494a0961c5a
Gael Lucero <gael@udel.edu>
2025-04-07 18:13:29 -0400

    Added basic TextBox

--- src/webpages/DetailedQuiz.tsx
+++ src/webpages/DetailedQuiz.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useState } from "react";
+import { FormControl, FormGroup, FormLabel } from "react-bootstrap";
 
 export function DetailedQuiz(): React.JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>){
+        setUserAnswer(event.target.value);
+    }
     return (
         <div>
             <h1>Detailed Quiz</h1>
+            <br />
+            <div>
+                <h3>1. Question 1</h3>
+            </div>
+            <div>
+            <FormGroup controlId="formUserAnswer" style={{backgroundColor: "royalblue"}}>
+                <FormLabel>Input Answer Below:</FormLabel>
+                <FormControl
+                    value={userAnswer}
+                    onChange={updateAnswer}
+                ></FormControl>
+            </FormGroup>
+            </div>
+            <div>
+                The text that the user has typed: {userAnswer}
+            </div>
         </div>
     )
 }

6836cc0fa6ba0c091ca712227d608bad11cfb7b2
aorlov05 <aorlov@udel.edu>
2025-04-29 13:24:34 -0400

    Merge quiz template

--- src/webpages/QuizPages/BasicQuiz.tsx
+++ src/webpages/QuizPages/BasicQuiz.tsx
@@ -4,6 +4,7 @@ import { Question } from "./Question";
 import { QuizTemplate } from "./QuizTemplate";
 import "./Quiz.css";
 import { PageType } from "../../pages";
+import OpenAI from 'openai';
 
 const BASIC_QUESTIONS: BasicQuizQuestion[] = [
     {
